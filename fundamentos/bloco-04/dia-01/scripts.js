9364f25b5532aa8928672d092446969379ba10e5
Pedro <booklyman@gmail.com>
2022-03-18 12:13:06 -0300

    bloco 4

---
+++ fundamentos/bloco-04/dia-01/scripts.js
@@ -0,0 +1,6 @@
+const myName = 'Sol';
+const birthCity = 'Natal';
+var birthYear = '2002';
+console.log(birthYear);
+var birthYear = '2030';
+console.log(birthYear);
---
+++ fundamentos/bloco-04/dia-01/switchCase.js
@@ -0,0 +1,15 @@
+let trafficLight;
+
+switch (TrafficLight) {
+    case "vermelho":
+        console.log("pare");
+        break;
+    case "amarelo":
+        console.log("atenção");
+        break;
+    case "verde":
+        console.log("siga");
+        break;
+    default:
+        console.log("valha, indoidiceu o bicho")
+}
